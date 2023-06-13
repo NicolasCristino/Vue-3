24c3e31b71153628bbf7683984675e547e524abc
Nicolas Cristino <nickpupu@gmail.com>
2023-06-13 17:30:14 -0300

    V-card

--- Vuetify/src/App.vue
+++ Vuetify/src/App.vue
@@ -8,9 +8,67 @@
       <v-container>
         <h1>Dashboard</h1>
         <v-row>
-          <v-col cols="12" md="4" lg="3">1</v-col>
-          <v-col cols="12" md="4" lg="3">1</v-col>
-          <v-col cols="12" md="4" lg="3">1</v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card>
+              <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                <v-card-title>Cidade perdida</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Wadi Musa</v-card-subtitle>
+
+              <v-card-text>
+                <div>Antiga Construção Em Pedra De Cidade Em Ruínas</div>
+                <div>Wadi Musa, Ma'an Governorate, Jordan</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card>
+              <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                <v-card-title>Cidade perdida</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Wadi Musa</v-card-subtitle>
+
+              <v-card-text>
+                <div>Antiga Construção Em Pedra De Cidade Em Ruínas</div>
+                <div>Wadi Musa, Ma'an Governorate, Jordan</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card>
+              <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                <v-card-title>Cidade perdida</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Wadi Musa</v-card-subtitle>
+
+              <v-card-text>
+                <div>Antiga Construção Em Pedra De Cidade Em Ruínas</div>
+                <div>Wadi Musa, Ma'an Governorate, Jordan</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-col>
+          
         </v-row>
       </v-container>
     </v-main>
