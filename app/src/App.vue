1578aafab50ee85be91c914b5be9d7197144b816
Nicolas Cristino <nickpupu@gmail.com>
2023-06-13 10:54:28 -0300

    Composition API Reatividade ref

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <AppProduct></AppProduct>
-  {{ name }}
+  <h5>User</h5>
+  {{ user.first_name }}
+  {{ user.last_name }}
+  <br><br>
+  <h5>Admin</h5>
+  {{ admin.first_name }}
+  {{ admin.last_name }}
+  <br><br>
   <img @click="changeName" alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import AppProduct from './components/Products/AppProduct.vue'
+import { ref, reactive } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld, AppProduct
+    HelloWorld
   },
   // No data() todos os componentes são reativos enquanto no setup não
   setup() {
+    const user = reactive({
+      first_name: 'Jon',
+      last_name: 'Snow'
+    })
+
+    const admin = ref({
+      first_name: 'Admin',
+      last_name: 'Master'
+    })
+
     let name = 'Nicolas'
 
     const changeName = () => {
       alert('Chegou')
       name = 'Felipe'
+      user.first_name = 'Sansa'
+      admin.value.first_name = 'Stark'
     }
 
     return {
-      name, changeName
+      name, changeName, user, admin
     }
   }
 }
@@ -35,7 +53,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: start;
   color: #2c3e50;
   margin-top: 60px;
 }
