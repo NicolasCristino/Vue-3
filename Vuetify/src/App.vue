30c5f4075a4be3467d620460be99f16447cce6d5
Nicolas Cristino <nickpupu@gmail.com>
2023-06-13 17:06:06 -0300

    Design GRID

--- Vuetify/src/App.vue
+++ Vuetify/src/App.vue
@@ -6,26 +6,17 @@
 
     <v-main>
       <v-container>
-        <v-btn color="purple-darken-3">Enviar</v-btn>
-        <h1 class="bg-blue-darken-3 text-h5 hidden-md-and-down">Dashboard</h1>
-
-        <div class="d-flex flex-column flex-md-row justify-space-between">
-          <div class="bg-grey-lighten-2 w-100 pa-4 border rounded-lg elevation-2">
-            teste
-          </div>
-          <div class="w-100 pa-4 border rounded-lg elevation-2">
-            teste
-          </div>
-          <div class="w-100 pa-4 border rounded-lg elevation-2">
-            teste
-          </div>
-        </div>
+        <h1>Dashboard</h1>
+        <v-row>
+          <v-col cols="12" md="4" lg="3">1</v-col>
+          <v-col cols="12" md="4" lg="3">1</v-col>
+          <v-col cols="12" md="4" lg="3">1</v-col>
+        </v-row>
       </v-container>
-      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '@/components/HelloWorld.vue' 
 </script>
