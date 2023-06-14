4a0c274fe77c101d3bf36d56c1a17818e60a6130
Nicolas Cristino <nickpupu@gmail.com>
2023-06-14 11:14:52 -0300

    Dashboard

--- Vuetify/src/App.vue
+++ Vuetify/src/App.vue
@@ -1,15 +1,69 @@
 <template>
-  <v-app>
+  <v-app theme="dark">
+    <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-subheader>Menu</v-list-subheader>
+        <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+        <v-list-item prepend-icon="mdi-account">Usuários</v-list-item>
+
+        <v-list-group value="Clientes">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              prepend-icon="mdi-account-circle"
+              title="Clientes"
+            ></v-list-item>
+          </template>
+
+          <v-list-item prepend-icon="mdi-currency-usd">Faturamento</v-list-item>
+          <v-list-item prepend-icon="mdi-note">Relatório</v-list-item>
+          <v-list-item prepend-icon="mdi-send">Emissão</v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar flat class="border-b">
+      <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
+
+      <template #append>
+        <v-btn icon class="mr-2">
+          <v-badge dot color="info">
+            <v-icon icon="mdi-bell-outline"></v-icon>
+          </v-badge>
+        </v-btn>
+
+        <v-menu>
+            <template #activator="{ props }">
+              <v-avatar v-bind="props">
+                <v-img cover src="https://i.pinimg.com/474x/f9/ce/18/f9ce1862e47d5134aa16728c06309f2e.jpg"></v-img>
+              </v-avatar>
+            </template>
+
+            <v-card min-width="200px">
+              <v-list nav density="compact" :lines="false"> 
+
+                <v-list-item prepend-icon="mdi-account-outline">
+                  <v-list-item-title>Meu perfil</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item prepend-icon="mdi-heart-outline">
+                  <v-list-item-title>Favoritos</v-list-item-title>
+                </v-list-item>
+
+              </v-list>
+            </v-card>
+        </v-menu>
+
+      </template>
       <v-app-bar-title>Meu app</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
       <v-container>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
         <v-row>
 
-          <v-col cols="12" md="4" lg="3">
+          <v-col cols="12" sm="6" md="4" lg="3">
             <v-card>
               <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
                 <v-card-title>Cidade perdida</v-card-title>
@@ -23,13 +77,14 @@
               </v-card-text>
 
               <v-card-actions>
-                <v-btn>Ver mais</v-btn>
+                <v-btn color="primary" variant="outlined">Ver mais</v-btn>
+                <v-btn color="blue" variant="tonal" prepend-icon="mdi-cart">Comprar</v-btn>
               </v-card-actions>
 
             </v-card>
           </v-col>
 
-          <v-col cols="12" md="4" lg="3">
+          <v-col cols="12" sm="6" md="4" lg="3">
             <v-card>
               <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
                 <v-card-title>Cidade perdida</v-card-title>
@@ -43,13 +98,14 @@
               </v-card-text>
 
               <v-card-actions>
-                <v-btn>Ver mais</v-btn>
+                <v-btn color="primary" variant="outlined">Ver mais</v-btn>
+                <v-btn color="blue" variant="tonal" prepend-icon="mdi-cart">Comprar</v-btn>
               </v-card-actions>
 
             </v-card>
           </v-col>
 
-          <v-col cols="12" md="4" lg="3">
+          <v-col cols="12" sm="6" md="4" lg="3">
             <v-card>
               <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
                 <v-card-title>Cidade perdida</v-card-title>
@@ -63,7 +119,29 @@
               </v-card-text>
 
               <v-card-actions>
-                <v-btn>Ver mais</v-btn>
+                <v-btn color="primary" variant="outlined">Ver mais</v-btn>
+                <v-btn color="blue"  variant="tonal" prepend-icon="mdi-cart">Comprar</v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img cover class="align-end text-white" src="https://images.pexels.com/photos/4388167/pexels-photo-4388167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                <v-card-title>Cidade perdida</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Wadi Musa</v-card-subtitle>
+
+              <v-card-text>
+                <div>Antiga Construção Em Pedra De Cidade Em Ruínas</div>
+                <div>Wadi Musa, Ma'an Governorate, Jordan</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn color="primary" variant="outlined">Ver mais</v-btn>
+                <v-btn color="blue"  variant="tonal" prepend-icon="mdi-cart">Comprar</v-btn>
               </v-card-actions>
 
             </v-card>
@@ -76,5 +154,7 @@
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue' 
+import { ref } from 'vue';
+
+  const isDrawerOpen = ref(false)
 </script>
